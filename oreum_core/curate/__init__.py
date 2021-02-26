36b47a55504076fd8af86d7e57b2c1a2c68d04ab
Jonathan Sedar <jon@sedar.co>
2021-02-26 17:34:02 +0900

    added csv2md convenience

--- oreum_core/curate/__init__.py
+++ oreum_core/curate/__init__.py
@@ -1,11 +1,14 @@
 # copyright 2021 Oreum OÜ
-from oreum_core.curate.data_load import PandasParquetIO
+from oreum_core.curate.data_load import (
+    PandasParquetIO, 
+    copy_csv2md
+)
 from oreum_core.curate.text_clean import (
     SnakeyLowercaser, 
     TextCleaner,
     StopWorder,
     NGrammer
-    )
+)
 from oreum_core.curate.data_transform import (
     DatatypeConverter,
     DatasetReshaper,
