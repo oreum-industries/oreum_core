a52a3f93b1b2a2172fec7cf8f1c480da45ff001b
Jonathan Sedar <jon@sedar.co>
2021-03-05 15:43:16 +0900

    added new functionality to split shit number reps

--- oreum_core/curate/text_clean.py
+++ oreum_core/curate/text_clean.py
@@ -2,7 +2,8 @@
 # copyright 2021 Oreum OÜ
 import ftfy
 import itertools
-import re
+import numpy as np
+import regex as re
 import requests
 import string
 import sys
@@ -87,7 +88,11 @@ class TextCleaner():
         self.nhs = re.compile(r'\d{3}\s?\d{3}\s?\d{4}')                         # UK NHS ID number
         # self.rx_nonchar = re.compile(r'[\d{}]+'.format(
         #     re.escape(string.punctuation)))
-        
+        self.rx_num_m = re.compile(r'^(?P<mill>[0-9]+?(?:[.]+?[0-9]+?)*?)m$', re.I)     # (1.4)M
+        self.rx_num_k = re.compile(r'^(?P<thou>[0-9]+?(?:[.]+?[0-9]+?)*?)k$', re.I)     # (400)k
+        self.rx_num = re.compile(r'^(?P<whol>[0-9]+?)(?P<frac>[.]+?[0-9]+?)*?$', re.I)  # (81)(.23)
+        self.rx_number_junk = re.compile(r'[#$€£₤¥,;%]')
+                
 
     def fix_unicode(self, txt):
         """ Fix bad unicode / emojis etc and try to remove crud """
@@ -114,6 +119,51 @@ class TextCleaner():
         t = self.rx_numbers.sub('', t)
 
         return t
+
+
+    def convert_bad_number_representation_to_float(self, s):
+        """ Accept a string that represents a number in a shitty way and convert to float
+            Issues: 
+                    Currently limited to k and M. 
+                    Hard to catch all usecases so returns nan on fail
+                    Yields % 100 too high
+            Corrects a multitide of sins e.g:
+                '1M' -> 1000000.
+                '1.4m' -> 1400000.
+                '25K' -> 25000.
+                '81.12k' -> 81120.
+                '3,000.82' -> 3000.82
+                '$3,000.82' -> 3000.82
+                '3.71%' -> 3.71
+
+            ts = ['1M', '1.4m', '25K', '81.12k', '400', '9,000.23', '$35,000.82', '3.71%']
+            for t in ts:
+                print(t, convert_bad_number_representation_to_float(t))           
+        """
+        r = np.nan
+        s0 = self.rx_number_junk.sub('', s.strip().lower())
+        gm = self.rx_num_m.match(s0)
+        gk = self.rx_num_k.match(s0)
+        gn = self.rx_num.match(s0)
+        
+        if gm is not None:
+            mill = gm.capturesdict()['mill']
+            mill = mill[0] if len(mill) > 0 else '0'
+            r = np.float64(f'{mill}') * 1e6
+
+        elif gk is not None:
+            thou = gk.capturesdict()['thou']
+            thou = thou[0] if len(thou) > 0 else '0'
+            r = np.float64(f'{thou}') * 1e3
+
+        elif gn is not None:
+            whol = gn.capturesdict()['whol']
+            whol = whol[0] if len(whol) > 0 else ''
+            frac = gn.capturesdict()['frac']
+            frac = frac[0] if len(frac) > 0 else '.0'
+            r = np.float64(f'{whol}') + np.float64(f'{frac}')
+            
+        return r
 
 
 class StopWorder():
