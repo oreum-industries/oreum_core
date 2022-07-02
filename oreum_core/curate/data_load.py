7046a54af9d98899c2fbacebcff68d4d32c4c9f2
Jonathan Sedar <jon@sedar.co>
2022-07-02 12:50:55 +0100

    minor improvements in curate.data_load
    + deprecated PandasParquetIO

--- oreum_core/curate/data_load.py
+++ oreum_core/curate/data_load.py
@@ -5,46 +5,18 @@ import os
 import subprocess
 import warnings
 
-import pandas as pd
 import pyarrow
 import pyarrow.parquet as pq
 
 
 class PandasParquetIO:
-    """Helper class to convert pandas to parquet and save to local path
-    and vice-versa
-
-    NOTE: this seems no longer needed see:
+    """Deprecated helper class to convert pandas to parquet and save to local
+    path and vice-versa. No longer needed see:
     https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-parquet
     """
 
-    def __init__(self, relpath=[]):
-        self.relpath = relpath
-        warnings.warn(
-            "This will be removed in future versions, use pandas to_parquet instead",
-            PendingDeprecationWarning,
-        )
-
-    def read_ppq(self, fn, relpath=[]):
-        if len(relpath) == 0:
-            relpath = self.relpath
-        fqn = os.path.join(*relpath, f'{fn}.parquet')
-        try:
-            arw_df = pq.read_table(fqn, use_pandas_metadata=True)
-        except OSError as e:
-            raise e
-        return pyarrow.Table.to_pandas(arw_df)
-
-    def write_ppq(self, df, fn, relpath=[]):
-        if len(relpath) == 0:
-            relpath = self.relpath
-        fqn = os.path.join(*relpath, f'{fn}.parquet')
-        arw_df = pyarrow.Table.from_pandas(df)
-        try:
-            pq.write_table(arw_df, fqn)
-        except FileNotFoundError as e:
-            raise e
-        return f'Written to {fqn}'
+    def __init__(self, **kwargs):
+        warnings.warn("Use pandas to_parquet instead", DeprecationWarning)
 
 
 class SimpleStringIO:
@@ -55,33 +27,35 @@ class SimpleStringIO:
         + 'json' to read/write dicts <-> json
     """
 
-    def __init__(self, relpath=[], kind='txt'):
+    def __init__(self, relpath: list = [], kind: str = 'txt'):
         self.relpath = relpath
         assert kind in set(['txt', 'json']), "kind must be in {'txt', 'json'}"
         self.kind = kind
 
-    def read(self, fn, relpath=[]):
+    def read(self, fn: str, relpath: list = []) -> str:
         if len(relpath) == 0:
             relpath = self.relpath
         fqn = os.path.join(*relpath, f'{fn}.{self.kind}')
         with open(fqn, 'r') as f:
             s = f.read()
+            f.close()
         if self.kind == 'json':
             s = json.loads(s)
         return s
 
-    def write(self, s, fn, relpath=[]):
+    def write(self, s: str, fn: str, relpath: list = []) -> str:
         if len(relpath) == 0:
             relpath = self.relpath
         fqn = os.path.join(*relpath, f'{fn}.{self.kind}')
         if self.kind == 'json':
             s = json.dumps(s)
         with open(fqn, 'w') as f:
-            f.write(s)
+            f.write(f'{s}\n')
+            f.close()
         return f'Written to {fqn}'
 
 
-def copy_csv2md(fqn):
+def copy_csv2md(fqn: str) -> str:
     """Convenience to copy csv 'path/x.csv' to markdown 'path/x.md'"""
     r = subprocess.run(['csv2md', f'{fqn}'], capture_output=True)
     with open(f'{fqn[:-3] + "md"}', 'wb') as f:
