dabb7fd20bbca1390a6b937d016ff115e298ae80
Jonathan Sedar <jon@sedar.co>
2022-07-03 15:03:48 +0100

    bugfix

--- oreum_core/model/plot.py
+++ oreum_core/model/plot.py
@@ -22,12 +22,13 @@ def plot_ppc_loopit(
     """Plot PPC & LOO-PIT after run `mdl.sample_posterior_predictive()` also see
     https://oriolabrilpla.cat/python/arviz/pymc3/2019/07/31/loo-pit-tutorial.html
     """
+    plotkind = 'kde' if kde else 'cumulative'
     f = plt.figure(figsize=(12, 6))
     gs = gridspec.GridSpec(2, 2, width_ratios=[1, 1], height_ratios=[2, 1], figure=f)
     ax0 = f.add_subplot(gs[0, :])
     ax1 = f.add_subplot(gs[2])
     ax2 = f.add_subplot(gs[3], sharex=ax1)
-    _ = az.plot_ppc(azid, kind='cumulative', flatten=None, ax=ax0)
+    _ = az.plot_ppc(azid, kind=plotkind, flatten=None, ax=ax0)
     _ = az.plot_loo_pit(azid, y='yhat', ax=ax1)
     _ = az.plot_loo_pit(azid, y='yhat', ecdf=True, ax=ax2)
     _ = f.suptitle('In-sample PPC Evaluation')
