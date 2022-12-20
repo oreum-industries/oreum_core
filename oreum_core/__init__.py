67e1bfa6bfeac7d83831d5576b3ce7b65ff1b616
Jonathan Sedar <jonsedar@users.noreply.github.com>
2022-12-20 12:35:35 +0000

    + added Path.resolve() throughout (#62)
    + minor improvements to plots

--- oreum_core/__init__.py
+++ oreum_core/__init__.py
@@ -1,6 +1,7 @@
 # copyright 2022 Oreum Industries
 import logging
 
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 
+# logger goes to null handler by default. importing packages can set elsewhere
 logging.getLogger('oreum_core').addHandler(logging.NullHandler())
