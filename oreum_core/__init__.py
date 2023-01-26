4e5b0583ae5be30a04418cbb7bb494bb662ed4dc
Jonathan Sedar <jon@sedar.co>
2023-01-26 11:51:32 +0000

    update version

--- oreum_core/__init__.py
+++ oreum_core/__init__.py
@@ -1,7 +1,8 @@
 # copyright 2022 Oreum Industries
+"""oreum_core"""
 import logging
 
-__version__ = "0.4.9"
+__version__ = "0.4.10"
 
 # logger goes to null handler by default. importing packages can set elsewhere
 logging.getLogger('oreum_core').addHandler(logging.NullHandler())
