07c78d46de023f33e96bc88c2e269c20e62085c6
Jonathan Sedar <jon@sedar.co>
2023-02-11 21:19:56 +0000

    sleep at 10 sec

--- oreum_core/__init__.py
+++ oreum_core/__init__.py
@@ -15,7 +15,7 @@
 """Core tools for use on projects by Oreum Industries"""
 import logging
 
-__version__ = "0.4.16a9"
+__version__ = "0.4.16a10"
 
 # logger goes to null handler by default
 # packages that import oreum_core can override this and direct elsewhere
