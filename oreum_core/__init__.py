bdf2b632b873f8e50a31edb29a09f606abc5f388
Jonathan Sedar <jon@sedar.co>
2023-03-13 16:33:07 +0400

    set ref_vals as kwargs

--- oreum_core/__init__.py
+++ oreum_core/__init__.py
@@ -15,7 +15,7 @@
 """Core tools for use on projects by Oreum Industries"""
 import logging
 
-__version__ = "0.4.18"
+__version__ = "0.4.19"
 
 # logger goes to null handler by default
 # packages that import oreum_core can override this and direct elsewhere
