ce1e59859c03ce13690076079961d81391ec60da
Jonathan Sedar <jon@sedar.co>
2023-09-20 17:51:59 +0400

    + moved plots

--- oreum_core/eda/plot.py
+++ oreum_core/eda/plot.py
@@ -842,62 +842,6 @@ def plot_r2_range_pair(r2_t, r2_pct_t, r2_h, r2_pct_h, lims=(0, 80)):
     _ = f.tight_layout()
 
 
-def plot_cdf_ppc_vs_obs(
-    y: np.ndarray, yhat: np.ndarray, xlim_max_override=None
-) -> figure.Figure:
-    """Plot (quantile summaries of) yhat_ppc vs y
-    NOTE:
-    y shape: (nobs,)
-    yhat shape: (nobs, nsamples)
-    """
-    ps = [3, 10, 20, 30, 40, 50, 60, 70, 80, 90, 97]
-    df_yhat_qs = pd.DataFrame(
-        np.percentile(yhat, ps, axis=1).T, columns=[f'q{p/100}' for p in ps]
-    )
-
-    f, axs = plt.subplots(1, 1, figsize=(14, 5), sharey=True, sharex=True)
-    _ = sns.kdeplot(
-        y, cumulative=True, lw=2, c='g', ax=axs, common_norm=False, common_grid=True
-    )
-
-    if df_yhat_qs.duplicated().sum() == len(df_yhat_qs) - 1:
-        # all dupes: model was intercept only
-        dfm = df_yhat_qs.iloc[:1].melt(var_name='ppc_q')
-        _ = sns.rugplot(
-            x='value',
-            hue='ppc_q',
-            data=dfm,
-            palette='coolwarm',
-            lw=2,
-            ls='-',
-            height=1,
-            ax=axs,
-            zorder=-1,
-        )
-    else:
-        dfm = df_yhat_qs.melt(var_name='ppc_q')
-        _ = sns.kdeplot(
-            x='value',
-            hue='ppc_q',
-            data=dfm,
-            cumulative=True,
-            palette='coolwarm',
-            lw=2,
-            ls='-',
-            ax=axs,
-            zorder=-1,
-            common_norm=False,
-            common_grid=True,
-        )
-
-    if xlim_max_override is not None:
-        _ = axs.set(xlim=(0, xlim_max_override), ylim=(0, 1))
-    else:
-        _ = axs.set(xlim=(0, np.ceil(y.max())), ylim=(0, 1))
-
-    _ = f.suptitle('Cumulative density plot of the posterior predictive vs actual')
-
-
 def plot_bootstrap_lr(
     dfboot: pd.DataFrame,
     df: pd.DataFrame,
@@ -1574,3 +1518,59 @@ def plot_grp_ct(
     _ = plt.tight_layout()
 
     return f
+
+
+def plot_cdf_ppc_vs_obs(
+    y: np.ndarray, yhat: np.ndarray, xlim_max_override=None
+) -> figure.Figure:
+    """Plot (quantile summaries of) yhat_ppc vs y
+    NOTE:
+    y shape: (nobs,)
+    yhat shape: (nobs, nsamples)
+    """
+    ps = [3, 10, 20, 30, 40, 50, 60, 70, 80, 90, 97]
+    df_yhat_qs = pd.DataFrame(
+        np.percentile(yhat, ps, axis=1).T, columns=[f'q{p/100}' for p in ps]
+    )
+
+    f, axs = plt.subplots(1, 1, figsize=(14, 5), sharey=True, sharex=True)
+    _ = sns.kdeplot(
+        y, cumulative=True, lw=2, c='g', ax=axs, common_norm=False, common_grid=True
+    )
+
+    if df_yhat_qs.duplicated().sum() == len(df_yhat_qs) - 1:
+        # all dupes: model was intercept only
+        dfm = df_yhat_qs.iloc[:1].melt(var_name='ppc_q')
+        _ = sns.rugplot(
+            x='value',
+            hue='ppc_q',
+            data=dfm,
+            palette='coolwarm',
+            lw=2,
+            ls='-',
+            height=1,
+            ax=axs,
+            zorder=-1,
+        )
+    else:
+        dfm = df_yhat_qs.melt(var_name='ppc_q')
+        _ = sns.kdeplot(
+            x='value',
+            hue='ppc_q',
+            data=dfm,
+            cumulative=True,
+            palette='coolwarm',
+            lw=2,
+            ls='-',
+            ax=axs,
+            zorder=-1,
+            common_norm=False,
+            common_grid=True,
+        )
+
+    if xlim_max_override is not None:
+        _ = axs.set(xlim=(0, xlim_max_override), ylim=(0, 1))
+    else:
+        _ = axs.set(xlim=(0, np.ceil(y.max())), ylim=(0, 1))
+
+    _ = f.suptitle('Cumulative density plot of the posterior predictive vs actual')
